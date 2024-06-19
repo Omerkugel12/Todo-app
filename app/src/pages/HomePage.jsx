cf629be78feb54656e7c9f45cf441141feafe830
Omerkugel12 <omerkugel12@gmail.com>
2024-06-19 08:03:14 +0300

    home-page-styling

--- app/src/pages/HomePage.jsx
+++ app/src/pages/HomePage.jsx
@@ -8,36 +8,42 @@ function HomePage() {
 
   return (
     <div className="home-container">
-      <div className="header-home-page-container">
-        <div className="header-home-page">
-          <h1>Welcome to My Todo List App</h1>
-          <p>
-            Keep track of your tasks and stay organized with our easy-to-use
-            todo list app.
-          </p>
+      <div className="header-and-statistics">
+        <div className="header-home-page-container">
+          <div className="header-home-page">
+            <h1>Welcome to My Todo List App</h1>
+            <p>
+              Keep track of your tasks and stay organized with our easy-to-use
+              todo list app.
+            </p>
+          </div>
         </div>
-      </div>
-
-      <div className="features">
-        <h2>Key Features</h2>
-        <ul>
-          <li>Manage tasks with ease</li>
-          <li>Organize tasks by categories</li>
-          <li>Set reminders and due dates</li>
-          <li>Track progress with statistics</li>
-        </ul>
-        <button>
-          <Link to="/todo" className="cta-button">
-            Get Started
-          </Link>
-        </button>
-      </div>
-
-      <div className="statistics">
-        <h2>Statistics</h2>
-        <p>Total Todos: {totalTodos}</p>
-        <p>Completed Todos: {completedTodos}</p>
-        <p>Active Todos: {activeTodos}</p>
+        <div className="features-wrapper">
+          <div className="features">
+            <h2>Key Features</h2>
+            <ul>
+              <li>Manage tasks with ease</li>
+              <li>Organize tasks by categories</li>
+              <li>Set reminders and due dates</li>
+              <li>Track progress with statistics</li>
+            </ul>
+            <div className="features-btn-wrapper">
+              <button className="features-btn">
+                <Link to="/todo" className="cta-button">
+                  Get Started
+                </Link>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="statistics-wrapper">
+          <div className="statistics">
+            <h2>Statistics</h2>
+            <p>Total Todos: {totalTodos}</p>
+            <p>Completed Todos: {completedTodos}</p>
+            <p>Active Todos: {activeTodos}</p>
+          </div>
+        </div>
       </div>
 
       <div className="navigation">
